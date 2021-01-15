708dee6a40db59c29f7ccb2b560b0f7e425fbeff
koheiyamayama <kohei.ccw.23@gmail.com>
2021-01-16 00:56:05 +0900

    Graphqlを用いたデータの取得方法(Query)を体験した

--- server.js
+++ server.js
@@ -1,20 +1,61 @@
-var { graphql, buildSchema } = require('graphql');
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
  
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName
+    this.age = age
+  }
+
+  fullName() {
+    return this.firstName + ' ' + this.lastName
+  }
+}
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type Person {
+    fullName: String!
+    firstName: String!
+    lastName: String!
+    age: Int!
+  }
+
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollDice(numDice: Int!, numSides: Int): [Int]
+    person(firstName: String!, lastName: String!, age: Int!): Person
   }
 `);
  
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
   },
+  random: () => {
+    return Math.random();
+  },
+  rollDice: ({numDice, numSides}) => {
+    const output = [];
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output
+  },
+  person: ({firstName, lastName, age}) => {
+    return new Person(firstName, lastName, age)
+  }
 };
  
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
